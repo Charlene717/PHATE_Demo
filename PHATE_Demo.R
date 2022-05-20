0062fd7adb9dcdad434e22c3377a6e39cb77a4b4
CharleneMSI <q2330416@gmail.com>
2022-05-20 11:59:16 +0800

    comment out other methods

--- PHATE_Demo.R
+++ PHATE_Demo.R
@@ -9,12 +9,13 @@ library(phateR)
 
 #Method1#
 seuratObject_Duc2 <-  seuratObject[,seuratObject@meta.data[["Cell_type"]]=="Ductal cell type 2"]
-#Method2#
-seuratObject_Duc2 <-  seuratObject[,seuratObject@meta.data[["Cell_type"]] %in% "Ductal cell type 2"]
 
-#Method3#
-# neurons_cds <- cds[,grepl("neurons", colData(cds)$assigned_cell_type, ignore.case=TRUE)]
-seuratObject_Duc2 <- seuratObject[,grepl("Ductal cell type 2", seuratObject@meta.data[["Cell_type"]], ignore.case=TRUE)]
+# #Method2#
+# seuratObject_Duc2 <-  seuratObject[,seuratObject@meta.data[["Cell_type"]] %in% "Ductal cell type 2"]
+# 
+# #Method3#
+# # neurons_cds <- cds[,grepl("neurons", colData(cds)$assigned_cell_type, ignore.case=TRUE)]
+# seuratObject_Duc2 <- seuratObject[,grepl("Ductal cell type 2", seuratObject@meta.data[["Cell_type"]], ignore.case=TRUE)]
 
 
 data <- seuratObject_Duc2@assays[["RNA"]]@counts %>% as.numeric()
